0f2daa73c8f7b619d8a2f8fa67602db73b3aa32a
darkyrepo <hexodark@videotron.ca>
2013-08-23 22:58:57 -0400

    Create conf.php

---
+++ box/conf.php
@@ -0,0 +1,6 @@
+<?php
+$url = 'http://'.$host.'/rutorrent/cakebox/';   //force le http
+$local_dl_path = '../../users/'.$user.'/';
+$dirpath = '/home/'.$user.'/torrents/';
+$onglet = true;                  //ou false
+?>
