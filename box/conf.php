18b7ad155bc61244204f0d2a2e857cab7f87db18
darkyrepo <hexodark@videotron.ca>
2013-08-23 23:51:23 -0400

    Update conf.php

--- box/conf.php
+++ box/conf.php
@@ -1,5 +1,5 @@
 <?php
-$url = 'http://'.$host.'/rutorrent/cakebox/';   //force le http
+$url = 'https://'.$host.'/rutorrent/cakebox/';   //force le http
 $local_dl_path = '../../users/'.$user.'/';
 $dirpath = '/home/'.$user.'/downloads/';
 $onglet = true;                  //ou false
