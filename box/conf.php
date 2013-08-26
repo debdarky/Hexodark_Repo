91c8856792f16a38615062994c8b2efd167a9b1c
darkyrepo <hexodark@videotron.ca>
2013-08-26 19:16:53 -0400

    Delete conf.php

--- box/conf.php
+++
@@ -1,6 +0,0 @@
-<?php
-$url = 'https://'.$host.'/rutorrent/cakebox/';   //force le http
-$local_dl_path = '../../users/'.$user.'/';
-$dirpath = '/home/'.$user.'/downloads/';
-$onglet = true;                  //ou false
-?>
